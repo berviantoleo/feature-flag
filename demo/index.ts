68fd687e784af55d92738bdac95054f454b661b0
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-04-13 23:19:20 +0700

    try to add recall and store token in memory

--- demo/index.ts
+++ demo/index.ts
@@ -1,5 +1,9 @@
 import featureFlag from "../src";
 
-featureFlag('try_feature_flag').then((result) => {
-    console.log(result);
-});
+for (let i of Array(50).fill(1).map((x, y) => x + y)) {
+  setTimeout(() => {
+    featureFlag("try_feature_flag").then((result) => {
+      console.log(result);
+    });
+  }, 5000 * i);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,55 @@
 import {
   AppConfigDataClient,
+  BadRequestException,
   GetLatestConfigurationCommand,
   StartConfigurationSessionCommand,
 } from "@aws-sdk/client-appconfigdata";
 
 const client = new AppConfigDataClient({});
+let existingToken: string;
 
-const featureFlag = async (flag: string): Promise<boolean> => {
+const getToken = async (): Promise<string> => {
   const getSession = new StartConfigurationSessionCommand({
     ApplicationIdentifier: process.env.APP_CONFIG_APP_IDENTIFIER,
-    ConfigurationProfileIdentifier: process.env.APP_CONFIG_CONFIG_PROFILE_IDENTIFIER,
+    ConfigurationProfileIdentifier:
+      process.env.APP_CONFIG_CONFIG_PROFILE_IDENTIFIER,
     EnvironmentIdentifier: process.env.APP_CONFIG_ENVIRONMENT_IDENTIFIER,
   });
   const sessionToken = await client.send(getSession);
-  const command = new GetLatestConfigurationCommand({
-    ConfigurationToken: sessionToken.InitialConfigurationToken,
-  });
-  const response = await client.send(command);
-  let flags: any = {};
-  if (response.Configuration) {
-    let str = "";
-    for (let i = 0; i < response.Configuration.length; i++) {
-      str += String.fromCharCode(response.Configuration[i]);
-    }
-    const allFlag = JSON.parse(str);
-    console.log(allFlag);
-    flags = Object.assign({}, allFlag);
+  return sessionToken.InitialConfigurationToken || "";
+};
+
+const featureFlag = async (flag: string): Promise<boolean> => {
+  if (!existingToken) {
+    existingToken = await getToken();
+    console.log(existingToken);
   }
-  return Boolean(flags[flag]?.enabled);
+  try {
+    const command = new GetLatestConfigurationCommand({
+      ConfigurationToken: existingToken,
+    });
+    const response = await client.send(command);
+    let flags: any = {};
+    if (response.Configuration) {
+      let str = "";
+      for (let i = 0; i < response.Configuration.length; i++) {
+        str += String.fromCharCode(response.Configuration[i]);
+      }
+      const allFlag = JSON.parse(str);
+      console.log(allFlag);
+      flags = Object.assign({}, allFlag);
+    }
+    return Boolean(flags[flag]?.enabled);
+  } catch (err) {
+    if (err instanceof BadRequestException) {
+      existingToken = await getToken();
+      console.log(existingToken);
+      // recall
+      return featureFlag(flag);
+    } else {
+      throw err;
+    }
+  }
 };
 
 export default featureFlag;
